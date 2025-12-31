c0a567539589775e2078c0285411cb3c6f72eb3b
SunnyMonster <itssunnymonster@gmail.com>
2025-12-31 23:40:26 +1300

    docs: improve comment

--- src/trampoline/happy_new_year.rs
+++ src/trampoline/happy_new_year.rs
@@ -4,7 +4,8 @@
 
 //! It is currently 11:30pm on Dec 31, 2025 and it is the last day I can label one of my code files
 //! as Copyright 2025 SunnyMonster. So here we go: a blank file that is totally completely useful!
-//! Future me can probably use this [`HAPPY_NEW_YEAR`](self::HAPPY_NEW_YEAR) constant as `()`.
+//! Future me can probably use this [`HAPPY_NEW_YEAR`](self::HAPPY_NEW_YEAR) constant as a
+//! replacement of `()`.
 //!
 //! Happy new year! :3
 
